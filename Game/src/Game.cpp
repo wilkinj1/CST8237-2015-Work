2650eef0502f6229438530c49ddff02c7ff65a84
Justin Wilkinson <wilkinsense@gmail.com>
2015-02-09 20:49:19 -0500

    Added test texture to show working with textures.

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -33,6 +33,27 @@ void Game::InitializeImpl()
   {
     (*itr)->Initialize(_renderer);
   }
+
+  // Load our texture.
+  _testTexture = IMG_LoadTexture(_renderer, "res/UV.jpg");
+  _testTextureAngleDegrees = 0.0f;
+
+  /* Query the texture to get it's size. We then use it's size to offset where the texture will draw relative to,
+   * since the default is it's top corner (0,0). */
+  _testTextureBounds = { 0, 0, 0, 0 };
+  SDL_QueryTexture(_testTexture, nullptr, nullptr, &_testTextureBounds.w, &_testTextureBounds.h);
+  _testTextureBounds.x -= _testTextureBounds.w / 2;
+  _testTextureBounds.y -= _testTextureBounds.h / 2;
+
+  /* This center will only be the point we rotate around; not the point we draw relative to. So if I set this center to be the middle
+  * of the texture, it will still draw at (0,0), and rotate around (250,250), which is where the middle of the texture would be. */
+  _testTextureRotationCenter = { _testTextureBounds.w / 2, _testTextureBounds.h / 2 };
+
+  // Get the window size so we can draw it in the center of the screen later.
+  SDL_Point windowSize;
+  SDL_GetWindowSize(_window, &windowSize.x, &windowSize.y);
+
+  _testTexturePosition = { windowSize.x / 2, windowSize.y / 2 };
 }
 
 void Game::UpdateImpl(float dt)
@@ -44,12 +65,23 @@ void Game::UpdateImpl(float dt)
   {
     (*itr)->Update(dt);
   }
+
+  // Increase the test texture's angle to get a continuous rotation.
+  _testTextureAngleDegrees += (10.0f * dt);
 }
 
 void Game::DrawImpl(SDL_Renderer *renderer, float dt)
 {
   std::vector<GameObject *> renderOrder;
   CalculateDrawOrder(renderOrder);
+  
+  /* Take the boundaries (-250,-250, 500, 500), and add the location of where we want to draw the texture to them so that it will be
+   * properly centered around that point. */
+  SDL_Rect drawRect = _testTextureBounds;
+  drawRect.x += _testTexturePosition.x;
+  drawRect.y += _testTexturePosition.y;
+
+  SDL_RenderCopyEx(renderer, _testTexture, nullptr, &drawRect, _testTextureAngleDegrees, &_testTextureRotationCenter, SDL_FLIP_NONE);
 
   for (auto itr = renderOrder.begin(); itr != renderOrder.end(); itr++)
   {
--- Game/src/Game.h
+++ Game/src/Game.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <GameEngine.h>
+#include <SDL_rect.h>
 
 union SDL_Event;
+struct SDL_Texture;
 
 class Game: public GameEngine
 {
@@ -20,4 +22,12 @@ protected:
 
   void Reset();
   void CalculateDrawOrder(std::vector<GameObject *>& drawOrder);
+
+  /* Test texture properties. */
+  SDL_Texture *_testTexture;
+  SDL_Rect _testTextureBounds;
+  float _testTextureAngleDegrees;
+
+  SDL_Point _testTextureRotationCenter;
+  Vector2 _testTexturePosition;
 };
